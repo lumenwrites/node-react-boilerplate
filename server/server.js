a62b6ee591b1cf4187001e318755b3d9cc1d2866
Ray Alez <raymestalez@gmail.com>
2018-11-24 18:33:07 -0800

    Update

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import path from 'path' // Manipulate filepaths
 import './connectDB'
 
 import profilesRoutes from './routes/profiles.js'
+import { stripeWebhook } from './controllers/profiles.js'
 
 
 /* Setup server */
@@ -14,6 +15,8 @@ const server = express()
 server.set('view engine', 'ejs')
 server.set('views', __dirname + '/views')
 server.use(cors())
+/* Stripe webhook must be used before the body-parser to validate correctly */
+server.use('/api/v1/profiles/stripe-webhook', stripeWebhook)
 /* Parse received JSON, and put it into req.body */
 server.use(bodyParser.json({ limit: '50mb' }))
 
